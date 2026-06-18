877760895cefac1ca4a67306cf123a07db3ab027
Eduardo Vignardi <eduardovignardi@gmail.com>
2026-06-17 21:51:42 -0300

    firebase debug

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -2,6 +2,16 @@ const admin = require("firebase-admin");
 
 const serviceAccount = require("./serviceAccountKey.json");
 
+console.log("================================");
+console.log("FIREBASE ADMIN");
+console.log("PROJECT ID:", serviceAccount.project_id);
+console.log("CLIENT EMAIL:", serviceAccount.client_email);
+console.log(
+  "PRIVATE KEY EXISTS:",
+  !!serviceAccount.private_key
+);
+console.log("================================");
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
